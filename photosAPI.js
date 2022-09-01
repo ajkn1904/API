bb64d735982280304c43f25caa57d1831c49506e
ajkn1904 <ajknishat@gmail.com>
2022-09-01 23:32:10 +0600

    update

---
+++ photosAPI.js
@@ -0,0 +1,47 @@
+const photoUrl = ('https://jsonplaceholder.typicode.com/photos');
+fetch(photoUrl)
+.then(res => res.json())
+.then (data => showPhotos(data));
+
+const showPhotos = (data) => {
+    
+data.slice(0, 16).forEach(element => {
+   // console.log(element);
+
+    const showPhoto = document.getElementById('showPhotos');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.style.width = '18rem';
+
+
+    div.innerHTML = `
+        <img src="${element.url}" class="card-img-top">
+        <div class="card-body">
+        <h5 class="card-title">${element.title}</h5>
+        <button class="btn btn-primary" onclick='shoewPhotoDetails(${JSON.stringify (element)})'>Show Details</button>
+        </div>
+    `
+    showPhoto.appendChild(div);
+    
+});
+}
+
+
+
+const shoewPhotoDetails = (data) => {
+    console.log(data);
+
+    const detailDiv = document.getElementById('detailsDiv');
+
+    detailDiv.innerHTML = `
+    <h2>Show Photo Details</h2>
+    <img src="${data.url} class="card-img-top">
+    <p>Thumbnail:</p>
+    <img src="${data.thumbnailUrl}">
+
+    <div class="card-body">
+    <h3 class="card-title">${data.title}</h3>
+    <p>ID: ${data.id}</p>
+    `
+}
+
