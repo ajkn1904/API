072baffdfd0238fc7daacb64c62c10abebafada0
ajkn1904 <ajknishat@gmail.com>
2022-09-02 20:30:37 +0600

    update

--- mobile-hunter.js
+++ mobile-hunter.js
@@ -1,43 +1,59 @@
-const loadPhone = (value) =>{
+const loadPhone = (value, dataLimit) =>{
     fetch(`https://openapi.programming-hero.com/api/phones?search=${value}`)
     .then(res => res.json())
-    .then(data => showMobile(data));
+    .then(data => showMobile(data.data, dataLimit));
 }
 
 
-const showMobile = (data) => {
+const showMobile = (data, dataLimit) => {
     //console.log(data.data);
     
     const showMobileDiv = document.getElementById('show-phone');
     showMobileDiv.innerHTML = '';
 
+    
+    const showAllField = document.getElementById('showAll');
+    if(dataLimit && data.length > 10){
+        data = data.slice(0, 10);
+        showAllField.classList.remove('d-none');
+    }
+    else{
+        showAllField.classList.add('d-none');
+    }
+    
+    
     const foundMsg = document.getElementById('notFoundMsg');
-    if(data.data.length === 0){
+    if(data.length === 0){
         foundMsg.classList.remove('d-none');
         showloader.classList.add('d-none');
+        showAllField.classList.add('d-none');
     }
     else{
         foundMsg.classList.add('d-none');
-        data.data.slice(0, 15).forEach(element => 
-            {          
-            const div = document.createElement('div');
-            div.classList.add('col');
-            
-            div.innerHTML = `
-            <div class="card">
-                <img src="${element.image}" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">${element.phone_name}</h5>
-                    <p>Brand: ${element.brand}</p>
-                    <button class="btn btn-outline-success" onclick="showDetails('${element.slug}') "data-bs-toggle="modal" data-bs-target="#mobileDetailsModal">Details</button>
-                </div>
-            </div>
-            `
-            showMobileDiv.appendChild(div);
-            }); 
-    showloader.classList.add('d-none');
-
     }
+
+        
+
+
+    data.forEach(element => 
+        {          
+        const div = document.createElement('div');
+        div.classList.add('col');
+            
+        div.innerHTML = `
+        <div class="card">
+            <img src="${element.image}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${element.phone_name}</h5>
+                <p>Brand: ${element.brand}</p>
+                <button class="btn btn-outline-success" onclick="showDetails('${element.slug}') "data-bs-toggle="modal" data-bs-target="#mobileDetailsModal">Details</button>
+            </div>
+        </div>
+        `
+        showMobileDiv.appendChild(div);
+        }); 
+    showloader.classList.add('d-none');
+   
 
 }
 
@@ -68,12 +84,34 @@ const loadModalData = (data) =>{
     `
 }
 
-const showloader = document.getElementById('loader');
-document.getElementById('searchPhoneBtn').addEventListener('click', function(){
+const processData = (dataLimit) =>{
     const textField = document.getElementById('searchPhone');
     const textFieldvalue = textField.value;
-    loadPhone(textFieldvalue);
+    loadPhone(textFieldvalue, dataLimit);
+    textField.value = '';
     showloader.classList.remove('d-none');
+}
+
+
+const showloader = document.getElementById('loader');
+
+document.getElementById('searchPhoneBtn').addEventListener('click', function(){
+    processData(10);
 
 });
-loadPhone('apple')
+
+document.getElementById('searchPhone').addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        processData(10)
+    }
+    
+});
+
+
+
+const btnShowAll = document.getElementById('showAllBtn').addEventListener('click', function(){
+    processData();
+});
+
+
+loadPhone('apple', 10)
