2813979685fd9685789861e58e297a5948e159d1
ajkn1904 <ajknishat@gmail.com>
2022-09-10 22:50:43 +0600

    update

--- Explore-Local-Storage/setItemToLocalsotrage.js
+++ Explore-Local-Storage/setItemToLocalsotrage.js
@@ -1,13 +1,20 @@
+function getItemFromLocalStorage(data){
+return localStorage.getItem(data);
+
+}
+
 let theCount = 0;
 document.getElementById('btn-increase').addEventListener('click', function(){
     let increaser = theCount++;
+    localStorage.setItem('the count', increaser);
+
+    const showData = getItemFromLocalStorage('the count');
     const addPara = document.getElementById('increase-paragraph');
     const li = document.createElement('li');
-    li.innerText = increaser;
+    li.innerText = showData;
     
     addPara.appendChild(li);
     
-    localStorage.setItem('the count', increaser);
 });
 
 
@@ -20,10 +27,12 @@ function valueFromInputField(id){
     return textFieldValue;
 }
 
-function showData(id, fieldValue){
+function showData(id, key){
+    const showValue = getItemFromLocalStorage(key);
+
     const addPara = document.getElementById(id);
     let paragraph = document.createElement('p')
-    paragraph.innerText = fieldValue;
+    paragraph.innerText = showValue;
     addPara.appendChild(paragraph);
 }
 
@@ -33,20 +42,42 @@ function setToLocalStorage(key, value){
 
 document.getElementById('btn-add-name').addEventListener('click', function(){
     const fieldValue = valueFromInputField('name-field');
-    showData('show-name-paragraph', fieldValue);
     setToLocalStorage('name', fieldValue);
+    showData('show-name-paragraph', 'name');
 });
 
 document.getElementById('btn-add-email').addEventListener('click', function(){
     const fieldValue = valueFromInputField('email-field');
-    showData('show-email-paragraph', fieldValue);
     setToLocalStorage('email', fieldValue);
+    showData('show-email-paragraph', 'email');
 });
 
 document.getElementById('btn-add-message').addEventListener('click', function(){
     const fieldValue = valueFromInputField('message-field');
-    showData('show-message-paragraph', fieldValue);
     setToLocalStorage('message', fieldValue);
+    showData('show-message-paragraph', 'message');
+});
+
+showData('show-name-paragraph', 'name');
+showData('show-email-paragraph', 'email');
+showData('show-message-paragraph', 'message');
+
+
+
+
+document.getElementById('btn-send').addEventListener('click', function(){
+    const nameFieldValue = valueFromInputField('name-field');
+    setToLocalStorage('name', nameFieldValue);
+    showData('show-name-paragraph', 'name');
+
+    const emailFieldValue = valueFromInputField('email-field');
+    setToLocalStorage('email', emailFieldValue);
+    showData('show-email-paragraph', 'email');
+
+    const messageFieldValue = valueFromInputField('message-field');
+    setToLocalStorage('message', messageFieldValue);
+    showData('show-message-paragraph', 'message');
+    
 });
 
 
@@ -63,47 +94,24 @@ function deletFromLocalStorage(key){
 }
 
 document.getElementById('btn-delet-name').addEventListener('click', function(){
-    deleteParagraphData('show-name-paragraph');
     deletFromLocalStorage('name');
+    deleteParagraphData('show-name-paragraph');
 });
 
 document.getElementById('btn-delet-email').addEventListener('click', function(){
-    deleteParagraphData('show-email-paragraph');
     deletFromLocalStorage('email');
+    deleteParagraphData('show-email-paragraph');
     
 });
 
 document.getElementById('btn-delet-message').addEventListener('click', function(){
-    deleteParagraphData('show-message-paragraph');
     deletFromLocalStorage('message');
+    deleteParagraphData('show-message-paragraph');
 });
-
-
-
-
-document.getElementById('btn-send').addEventListener('click', function(){
-    const nameFieldValue = valueFromInputField('name-field');
-    showData('show-name-paragraph', nameFieldValue);
-    setToLocalStorage('name', nameFieldValue);
-
-    const emailFieldValue = valueFromInputField('email-field');
-    showData('show-email-paragraph', emailFieldValue);
-    setToLocalStorage('email', emailFieldValue);
-
-    const messageFieldValue = valueFromInputField('message-field');
-    showData('show-message-paragraph', messageFieldValue);
-    setToLocalStorage('message', messageFieldValue);
-    
-});
-
-
-
-
 
 document.getElementById('btn-reset').addEventListener('click', function(){
     window.localStorage.clear();
-    const addPara = document.getElementById('increase-paragraph');
-    addPara.innerHTML = '';
+
     deleteParagraphData('show-name-paragraph')
     deleteParagraphData('show-email-paragraph');
     deleteParagraphData('show-message-paragraph');
